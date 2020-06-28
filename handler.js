eac4bb32f42b7c2ed998edba26d560d5fa2a063b
Grant McDonald <gmcdonald@amuniversal.com>
2020-06-28 12:08:33 -0500

    Stubbing in Ip Functions

--- handler.js
+++ handler.js
@@ -1,9 +1,13 @@
 export const hello = async (event, context) => {
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // (* or a specific host)
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
     body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!'}))}`,
-    }),
+      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`
+    })
   };
 };
 
@@ -12,3 +16,29 @@ const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
     resolve(`${rest.copy} (with a delay)`);
   }, time * 1000)
 );
+
+export const geoIp = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // (* or a specific host)
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({
+      message: `Go GeoIp Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`
+    })
+  };
+};
+
+export const whoIs = async (event, context) => {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*", // (* or a specific host)
+      "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
+    },
+    body: JSON.stringify({
+      message: `Go WhoIs Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`
+    })
+  };
+};
