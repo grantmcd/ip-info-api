ae9d32837a96e29808e89b4dddb97465ac22d741
Grant McDonald <gmcdonald@amuniversal.com>
2020-06-28 20:42:01 -0500

    Adding tests

--- handler.js
+++ handler.js
@@ -3,22 +3,6 @@ const CorsHeaders = {
   "Access-Control-Allow-Credentials": true // Required for cookies, authorization headers with HTTPS
 };
 
-export const hello = async (event, context) => {
-  return {
-    statusCode: 200,
-    headers: CorsHeaders,
-    body: JSON.stringify({
-      message: `Go Serverless v1.0! ${(await message({ time: 1, copy: 'Your function executed successfully!' }))}`
-    })
-  };
-};
-
-const message = ({ time, ...rest }) => new Promise((resolve, reject) =>
-  setTimeout(() => {
-    resolve(`${rest.copy} (with a delay)`);
-  }, time * 1000)
-);
-
 export const geoIp = async (event, context) => {
   const maxmind = require('maxmind');
   const lookup = await maxmind.open('./data/GeoLite2-City.mmdb');
@@ -32,7 +16,7 @@ export const geoIp = async (event, context) => {
   };
 };
 
-export const whoIs = async (event, context) => {
+export const whoisIp = async (event, context) => {
   const whoiser = require('whoiser');
 
   let whoisInfo = await whoiser(event.pathParameters.ipAddress);
--- tests/handler.test.js
+++ tests/handler.test.js
@@ -1,12 +1,39 @@
 import * as handler from '../handler';
 
-test('hello', async () => {
-  const event = 'event';
+test('geoIp', async () => {
+  const event = {
+    pathParameters: {
+      ipAddress: "75.34.225.8"
+    }
+  };
+
   const context = 'context';
   const callback = (error, response) => {
     expect(response.statusCode).toEqual(200);
     expect(typeof response.body).toBe("string");
+
+    const geoIpData = JSON.parse(response.body);
+    expect(geoIpData.city.name.en).toEqual("Shawnee");
   };
 
-  await handler.hello(event, context, callback);
+  await handler.geoIp(event, context, callback);
+});
+
+test('whoisIp', async () => {
+  const event = {
+    pathParameters: {
+      ipAddress: "75.34.225.8"
+    }
+  };
+
+  const context = 'context';
+  const callback = (error, response) => {
+    expect(response.statusCode).toEqual(200);
+    expect(typeof response.body).toBe("string");
+
+    const whoisData = JSON.parse(response.body);
+    expect(whoisData.Organization).toEqual("AT&T Corp. (AC-3280)");
+  };
+
+  await handler.whoisIp(event, context, callback);
 });
